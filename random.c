fb1ba723359eff61d3e7767be931fb8d2be2bba5
Alexei A. Smekalkine <ikle@ikle.ru>
2021-09-12 12:27:15 +0300

    random: revert magic, keep implementation simple and secure

--- random.c
+++ random.c
@@ -11,16 +11,11 @@
 
 #include <openssl/rand.h>
 
-#include <capi/hash.h>
 #include <capi/random.h>
 
 struct capi_random {
 	struct capi *core;
-
-	union {
-		unsigned long long state;
-		struct capi_hash *prf;
-	};
+	unsigned long long state;
 };
 
 struct capi_random *capi_random_alloc (struct capi *core)
@@ -33,22 +28,13 @@ struct capi_random *capi_random_alloc (struct capi *core)
 	o->core  = core;
 	o->state = 1;
 
-	if (core != NULL && (o->prf = capi_hash_alloc (core, "sha256")) == NULL)
-		goto no_prf;
-
 	return o;
-no_prf:
-	free (o);
-	return NULL;
 }
 
 void capi_random_free (struct capi_random *o)
 {
 	if (o == NULL)
 		return;
-
-	if (o->core != NULL)
-		capi_hash_free (o->prf);
 
 	free (o);
 }
@@ -76,31 +62,12 @@ static int simple_random (struct capi_random *o, void *data, size_t len)
 static int secure_seed (struct capi_random *o, const void *data, size_t len)
 {
 	RAND_seed (data, len);
-
-	return capi_hash_update (o->prf, data, len);
+	return 1;
 }
 
 static int secure_random (struct capi_random *o, void *data, size_t len)
 {
-	const size_t hs = capi_hash_size (o->prf);
-	unsigned char h[hs], *p;
-
-	if (RAND_bytes (h, hs) != 1)
-		return 0;
-
-	for (p = data; len > hs; p += hs, len -= hs) {
-		capi_hash_update (o->prf, h, hs);
-		capi_hash_update (o->prf, &len, sizeof (len));
-		capi_hash_fetch  (o->prf, h, hs);
-		memcpy (p, h, hs);
-	}
-
-	capi_hash_update (o->prf, h, hs);
-	capi_hash_update (o->prf, &len, sizeof (len));
-	capi_hash_fetch  (o->prf, h, hs);
-	memcpy (p, h, len);
-
-	return 1;
+	return RAND_bytes (data, len) == 1;
 }
 
 int capi_randon_seed (struct capi_random *o, const void *data, size_t len)
